eeb46859d97a906ec2209443f119ea91c896a36c
Andy Chen <neveray0932@gmail.com>
2022-03-17 17:22:47 +0800

    加上ApiModelProperty

--- src/main/java/com/neveray0932/fengchai/common/dto/employee/EmployeeUpdateDto.java
+++ src/main/java/com/neveray0932/fengchai/common/dto/employee/EmployeeUpdateDto.java
@@ -1,7 +1,10 @@
 package com.neveray0932.fengchai.common.dto.employee;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.neveray0932.fengchai.common.Vo.ApiMsg;
 import com.neveray0932.fengchai.common.dto.DTOEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,25 +12,35 @@ import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@ApiModel(value = "EmployeeUpdate對象",
+        description = ApiMsg.API_DESCRIPTION)
 public class EmployeeUpdateDto implements DTOEntity {
+    @ApiModelProperty(value = "員工編號",required = true)
     private String empId;
 
+    @ApiModelProperty(value = "員工姓名",required = true)
     private String empName;
 
+    @ApiModelProperty(value = "員工身分證")
     private String empIdentityCard;
 
+    @ApiModelProperty(value = "員工電話")
     private String empPhone;
 
+    @ApiModelProperty(value = "員工地址")
     private String empAddress;
 
+    @ApiModelProperty(value = "員工有無帳號",notes = "預設0")
     private Integer empUsernameFlag;
 
     @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
+    @ApiModelProperty(value = "到職時間")
     private Date createTime;
 
     @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
+    @ApiModelProperty(value = "離職時間")
     private Date empLeaveTime;
 
-
+    @ApiModelProperty(value = "職位")
     private String empPositionName;
 }
--- src/main/java/com/neveray0932/fengchai/common/dto/product/ProductCreateDto.java
+++ src/main/java/com/neveray0932/fengchai/common/dto/product/ProductCreateDto.java
@@ -1,41 +1,46 @@
 package com.neveray0932.fengchai.common.dto.product;
 
 
+import com.neveray0932.fengchai.common.Vo.ApiMsg;
 import com.neveray0932.fengchai.common.dto.DTOEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@ApiModel(value = "ProductCreate對象",
+        description = ApiMsg.API_DESCRIPTION)
 public class ProductCreateDto implements DTOEntity {
 
-
+    @ApiModelProperty(value = "產品ID",required = true)
     private String pId;
 
-
+    @ApiModelProperty(value = "產品名稱")
     private String pName;
 
-
+    @ApiModelProperty(value = "產品單位名稱")
     private String pUnitName;
 
-
+    @ApiModelProperty(value = "產品價格")
     private Integer pPrice;
 
-
+    @ApiModelProperty(value = "產品係數")
     private Float pCoefficient;
 
-
+    @ApiModelProperty(value = "產品成本")
     private Integer pCost;
 
-
+    @ApiModelProperty(value = "產品尺寸")
     private String pSize;
 
-
+    @ApiModelProperty(value = "供應商名稱")
     private String compName;
 
-
+    @ApiModelProperty(value = "產品備註")
     private String pRemark;
 
-
+    @ApiModelProperty(value = "邏輯刪除")
     private Integer delFlag;
 }
